ae61e7a404f3123a1cd79e21721ffb0c253d1d09
Octopus Low Tech <octopuslowtech@gmail.com>
2024-11-27 14:03:49 +0700

    backup

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
 
       const pc = new RTCPeerConnection({
         iceServers: [
-          { urls: "stun:stun.l.google.com:19302" },
+          // { urls: "stun:stun.l.google.com:19302" },
           { 
             urls: "turn:turn.onox.pro:3478",
             username: "octopus",
